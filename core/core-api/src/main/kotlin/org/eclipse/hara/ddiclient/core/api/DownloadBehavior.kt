2d13b27f35b149f864feaa58e86aca8692fe68b0
Daniele Sergio <daniele.sergio@kynetics.com>
2022-03-14 14:53:10 +0100

    Update kdoc of DownloadBehavior
    Signed-off-by: Daniele Sergio <daniele.sergio@kynetics.com>

--- core/core-api/src/main/kotlin/org/eclipse/hara/ddiclient/core/api/DownloadBehavior.kt
+++ core/core-api/src/main/kotlin/org/eclipse/hara/ddiclient/core/api/DownloadBehavior.kt
@@ -6,30 +6,31 @@ package org.eclipse.hara.ddiclient.core.api
 interface DownloadBehavior {
 
     /**
-     * Method that defines if the client should execute a download or not.
-     * @param attempt next download attempt for the artifact identified by [artifactId]
+     * Method that defines whether the client should execute a download or not.
+     * @param attempt index of the next download attempt for the artifact identified by [artifactId].
+     * Its value is in the [1, 2147483647] range, any attempt after 2147483647 will have index
+     * 2147483647.
      * @param artifactId
-     * @param previousError the error that has made fail the previous download attempt
-     *
+     * @param previousError the error that caused the failure of the previous download attempt
      * @return [Try]
      */
     fun onAttempt(attempt:Int, artifactId:String, previousError:Throwable? = null):Try
 
     /**
-     * Class that specified if the client should do another download attempt
+     * Class that specifies whether the client should do another download attempt
      */
     sealed class Try{
 
         /**
-         * The client have to stop to try downloading the artifact
+         * The client has to stop trying to download the artifact
          */
         object Stop:Try()
 
         /**
-         * The client have to try download a file after [seconds] seconds
+         * The client has to try downloading the artifact after [seconds] seconds
          *
          * @param seconds the time in seconds that the client has to wait
-         * before tries download the artifact
+         * before trying a download attempt
          */
         data class After(val seconds:Long):Try()
     }
