93c62178a87bbb01e118b0da618057ca2801e864
Daniele Sergio <daniele.sergio@kynetics.com>
2023-03-14 09:46:39 +0100

    Update Copyright

--- ddi-consumer/src/main/kotlin/org/eclipse/hara/ddi/api/DdiRestConstants.kt
+++ ddi-consumer/src/main/kotlin/org/eclipse/hara/ddi/api/DdiRestConstants.kt
@@ -1,12 +1,12 @@
 /*
-* Copyright © 2017-2021  Kynetics  LLC
-*
-* This program and the accompanying materials are made
-* available under the terms of the Eclipse Public License 2.0
-* which is available at https://www.eclipse.org/legal/epl-2.0/
-*
-* SPDX-License-Identifier: EPL-2.0
-*/
+ * Copyright © 2017-2023  Kynetics  LLC
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package org.eclipse.hara.ddi.api
 
 /**
